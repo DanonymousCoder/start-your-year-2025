1ac1756b6248178dd3e861ba7d6745f5d45d39e4
Ridwanullah Baruwa <128502434+DanonymousCoder@users.noreply.github.com>
2025-01-22 20:19:09 +0100

    Update data-types.js

--- javascript/data-types.js
+++ javascript/data-types.js
@@ -2,6 +2,7 @@ let variable = "foo"; // this is a string
 variable = 0; // variable is reassigned as a number
 variable = false; // variable is reassigned as a boolean
 variable = 0.1; // variable is reassigned as a number
+variable = "fooling around";
 
 let foo = variable + 12;
 foo = foo + " this is twelve and ";
